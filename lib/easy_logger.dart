e71dcfe07eff10188cf1ed246af02498e1fc5fde
Giovanni Bruno <giovanni.bruno@cyberneid.com>
2023-11-06 12:53:46 +0100

    First commit

---
+++ lib/easy_logger.dart
@@ -0,0 +1,17 @@
+library easy_logger;
+
+class Logger {
+  static LogMode _logMode = LogMode.debug;
+
+  static void init(LogMode mode) {
+    Logger._logMode = mode;
+  }
+
+  static void log(dynamic data, {StackTrace? stackTrace}) {
+    if (_logMode == LogMode.debug) {
+      print("$data$stackTrace");
+    }
+  }
+}
+
+enum LogMode { debug, live }
